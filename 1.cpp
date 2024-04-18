a0ed45f1639170a697c2732fa9ebd57403cf2908
priyanshu <jainpriyanshu20@gmail.com>
2024-04-18 11:31:19 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=14;
+    if(a&1)
+    {
+        cout<<a<<" is an odd number";
+    }
+    else
+    {
+        cout<<a<<" is an even number";
+    }
+ 
+}
---
+++ signedunsigned.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    signed char a=1;
+    //unsigned char a=1;
+    //unsigned int a=1;
+    //unsigned short int a=1;
+    //unsigned long int a=1;
+
+    while(a)
+    {
+        printf("%d \t", a);
+        a++;
+    }
+
+}
