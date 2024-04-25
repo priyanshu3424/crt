b19d04fae11daf4ff0a891a4a12d76c82cf67231
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-04-25 11:04:48 +0530

    Add files via upload

---
+++ bitset2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main()  {
+
+    cout<<"-256 = "<<bitset <8>(-256)<<endl;
+    cout<<"255 = "<<bitset <8>(255)<<endl;
+    cout<<"~255 = "<<bitset <8>(~255)<<endl;
+
+
+}
