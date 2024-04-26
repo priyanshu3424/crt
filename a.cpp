da42064026f85b361619b659b8fba8c5ba278562
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-04-26 10:55:15 +0530

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void hi();
+void bye();
+
+int main() {
+    cout << "Welcome at jaipur" << endl;
+    hi();
+    bye();
+}
+
+void hi() {
+    cout << "Hi vikas" << endl;
+}
+
+void bye() {
+    cout << "Bye vikas" << endl;
+}
