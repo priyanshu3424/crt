08f5795581273576df6effe81d0db107542e246e
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-01 23:53:25 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -6,13 +6,11 @@ using namespace std;
 bool isPalindrome(const string& str) {
     return equal(str.begin(), str.begin() + str.size() / 2, str.rbegin());
 }
-
 int main() {
     string text;
 
     cout << "Input a string: ";
     cin >> text;
-    // Alternatively, you can use getline(cin, text) to input a string with spaces.
 
     bool palindrome = isPalindrome(text);
 
