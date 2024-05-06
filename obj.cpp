ae6bd23d59f33efe561d4a9e4eb77ca99598ab6e
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-06 16:41:20 +0530

    Add files via upload

---
+++ obj.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+// class test
+// {
+//     public:
+//     int a=90;
+//     void show()
+//     {
+//         cout<<a;
+//     }
+// };
+// int main()
+// {
+//     test t;
+//     t.show();
+// }
+
+//----------------------------------------------------
+
+// class test
+// { 
+//     public:
+//     int roll;  // global variable scope whole program
+//     void insert() 
+//     {
+//         int r;
+//         roll = r;  // r is a local variable
+//     }
+//     void display()
+//     {
+//         cout<<roll;
+//     }
+// };
+// int main()
+// {
+//     test t;
+//     t.insert();
+//     t.display();
+// }
+
