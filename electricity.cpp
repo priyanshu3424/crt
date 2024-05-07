ca25b7e81282bf29d8d55f861452d51cd9bfae3c
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-07 10:36:54 +0530

    Add files via upload

---
+++ electricity.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int units;
+    int cost=0;
+    
+    cout <<"Enter the units: ";
+    cin >> units;
+
+    if(units<=100){
+        cost = 200;
+    }
+
+     else if(units<=200){
+        cost = 200 + (units-100)*2;
+    }
+
+     else if(units<=300){
+        cost = 200 + (units-200)*3;
+    }
+
+     else if(units<=400){
+        cost = 200 + (units-300)*5;
+    }
+
+     else if (units>400) {
+        cost = 200 + (units-400)*7;
+    }
+
+
+    cout << "Total cost: " << cost + 200 << endl;
+    return 0;
+}
