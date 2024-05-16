8bff55d3a3ad92ce271f70212a8424fab71d64cb
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-16 20:47:17 +0530

    Update permutations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -1,24 +1,18 @@
-// Recursive function to generate all permutations of a string
 #include <iostream>
 #include <string>
 using namespace std;
 
 void generate_Permutations(string text, int left, int right) 
 {
-  // Base case: if left and right indices are the same, 
-  // we have a complete permutation
   if (left == right) 
-  {	 cout << text << endl; // Output the generated permutation
+  {	 cout << text << endl; 
      return;
-  }
-
-  // Recursive case: swap each character with the character at the left index,
-  // and recursively generate permutations for the rest of the string
+  
   for (int i = left; i <= right; i++) 
   {
-    swap(text[left], text[i]); // swap characters 
-    generate_Permutations(text, left + 1, right); // Recursively generate permutations
-    swap(text[left], text[i]); // Backtrack by swapping back the characters
+    swap(text[left], text[i]); 
+    generate_Permutations(text, left + 1, right);
+    swap(text[left], text[i]);
   }
 }
 
@@ -29,7 +23,7 @@ int main()
   cin >> text;
 
   cout << "All permutations of the string: " << endl;
-  generate_Permutations(text, 0, text.length() - 1); // Generate permutations
+  generate_Permutations(text, 0, text.length() - 1);
 
   return 0;
 }
