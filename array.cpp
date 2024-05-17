ee9a06a927360df460665c1675de227c378e98ef
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-17 14:53:32 +0530

    Add files via upload

--- array.cpp
+++ array.cpp
@@ -1,10 +1,31 @@
-#include <stdio.h>
-int main()
-{
+#include <bits/stdc++.h>
+using namespace std;
 
-	int arr_int[5];
-    
-	char arr_char[5];
+void printArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
-	return 0;
+void swapElements(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int main() {
+    int arr[5] = {2, 4, 5, 6, 7};
+    int size = 5;
+
+    cout << "Array before swap: ";
+    printArray(arr, size);
+
+    // Swap the first and last elements
+    swapElements(arr, 0, size - 1);
+
+    cout << "Array after swap: ";
+    printArray(arr, size);
+
+    return 0;
 }
